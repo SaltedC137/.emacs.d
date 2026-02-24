f5f3d178df5b2f9baca3fc79e7b316c10a92c8d4
ArcadeFire <16298975+arcadefire@user.noreply.gitee.com>
2026-02-24 15:30:59 +0800

    2026-2-24 15:30:57

--- lisp/acs-package.el
+++ lisp/acs-package.el
@@ -7,6 +7,21 @@
 ;;   processing the init file but after processing the early init
 ;;   file.
 
+(setopt package-quickstart nil  ; 每次启动时 re-computing 而不是 使用 precomputed 的文件.
+        ;; 相当于在 加载“init.el” 前 执行‘package-initialize’.
+        ;; 这其实是 默认行为.
+        package-enable-at-startup t)
+
+(acs/custom:appdata/ package-user-dir /)
+
+(setopt network-security-level 'low)
+
+(setopt package-archives '(
+                        ("gnu"    . "https://mirrors.ustc.edu.cn/elpa/gnu/")
+                        ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")
+                        ("melpa"  . "https://mirrors.ustc.edu.cn/elpa/melpa/")
+                        ))
+
 (provide 'acs-package)
 
 ;; Local Variables:
