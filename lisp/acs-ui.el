26c5a1eee33fff1671fda43d333293f4d0a1ea5b
SallyFace <saltedc137@gmail.com>
2026-03-10 17:16:57 +0800

    Littel Fix Tab Line

--- lisp/acs-ui.el
+++ lisp/acs-ui.el
@@ -262,19 +262,17 @@
 
 
 
-;; (custom-set-faces
-;;  '(tab-line
-;;    ((t (:inherit mode-line-inactive :background "black" :height 1.0 :box nil))))
-;;  '(tab-line-tab
-;;    ((t (:inherit tab-line :background "gray20" :foreground "gray70" :box (:line-width 1 :color "gray20")))))
-;;  '(tab-line-tab-inactive
-;;    ((t (:inherit tab-line-tab))))
-;;  '(tab-line-tab-current
-;;    ((t (:background "SlateBlue4" :foreground "white" :weight bold :box (:line-width 1 :color "SlateBlue4")))))
-;;  '(tab-line-highlight
-;;    ((t (:background "dark slate gray" :foreground "white")))))
-
-
+(custom-set-faces
+ '(tab-line
+   ((t (:inherit mode-line-inactive :background "black" :height 1.0 :box nil))))
+ '(tab-line-tab
+   ((t (:inherit tab-line :background "gray20" :foreground "gray70" :box (:line-width 1 :color "gray20")))))
+ '(tab-line-tab-inactive
+   ((t (:inherit tab-line-tab))))
+ '(tab-line-tab-current
+   ((t (:background "SlateBlue4" :foreground "white" :weight bold :box (:line-width 1 :color "SlateBlue4")))))
+ '(tab-line-highlight
+   ((t (:background "dark slate gray" :foreground "white")))))
 
 
 (global-tab-line-mode t)
